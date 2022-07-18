d79e09e234b208ed6b5fec1ddba7e88f8be4520d
kholoud <kholoud.13en@gmail.com>
2022-07-18 13:53:53 +0200

    INIT

---
+++ backend/src/test/java/com/application/SDM/SdmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.SDM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
